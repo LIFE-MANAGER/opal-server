911aeb896deacf32bbde2b4e7b739a02346c70f6
BOK-MINJEONG <129048011+BOK-MINJEONG@users.noreply.github.com>
2023-07-27 15:35:10 +0900

    start project

---
+++ src/main/java/com/lifeManager/opalyouth/OpalYouthApplication.java
@@ -0,0 +1,13 @@
+package com.lifeManager.opalyouth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpalYouthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpalYouthApplication.class, args);
+	}
+
+}
