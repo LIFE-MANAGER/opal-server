96a7f3ec99647b021f22f1030bb15586bf852565
DongchannN <ldc991104@gmail.com>
2023-08-06 23:23:16 +0900

    #5-BaseResponseStatus 추가

--- src/main/java/com/lifeManager/opalyouth/common/response/BaseResponseStatus.java
+++ src/main/java/com/lifeManager/opalyouth/common/response/BaseResponseStatus.java
@@ -16,13 +16,18 @@ public enum BaseResponseStatus {
      */
     NO_AUTH(false, HttpStatus.UNAUTHORIZED.value(), "권한이 없습니다."),
     EXIST_EMAIL(false, HttpStatus.CONFLICT.value(), "이미 존재하는 회원입니다"),
+    INACTIVE_ACCOUNT(false, HttpStatus.BAD_REQUEST.value(), "휴면 계정입니다."),
     NON_EXIST_USER(false, HttpStatus.NOT_FOUND.value(), "존재하지 않는 회원입니다"),
+    WRONG_PASSWORD(false, HttpStatus.BAD_REQUEST.value(), "잘못된 비밀번호 입니다."),
+    EXPIRED_JWT_TOKEN(false, HttpStatus.UNAUTHORIZED.value(), "만료된 토큰입니다"),
+    INVALID_JWT_TOKEN(false, HttpStatus.NOT_ACCEPTABLE.value(), "유효하지 않은 토큰입니다."),
 
 
     /**
      * Server Error - 5xx 에러
      */
-    DATABASE_INSERT_ERROR(false, HttpStatus.INTERNAL_SERVER_ERROR.value(), "데이터베이스 저장에 실패하였습니다.")
+    DATABASE_INSERT_ERROR(false, HttpStatus.INTERNAL_SERVER_ERROR.value(), "데이터베이스 저장에 실패하였습니다."),
+    INTERNAL_SERVER_ERROR(false, HttpStatus.INTERNAL_SERVER_ERROR.value(), "알 수 없는 에러가 발생하였습니다."),
     ;
 
     private final boolean isSuccess;
